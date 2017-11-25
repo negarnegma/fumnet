edf74395bbdf7d9aa411ac301299c1f3dc8ca66d
negarnegma <negarzist@gmail.com>
2017-11-25 15:07:24 +0330

    remembering loginData, fixed some errors

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -33,7 +33,8 @@ angular.module('starter.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
+    window.localStorage.setItem("savedusername", $scope.loginData.username);
+    window.localStorage.setItem("savedpass", $scope.loginData.password);
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
     $timeout(function() {
@@ -56,20 +57,22 @@ function parseHTML(html) {
 
 
   $ionicPlatform.ready(function(){
+    var storage = window.localStorage;
     $scope.matn = "سلام!"
     $scope.currentStatus = "کللیک کن";if (!$scope.$$phase) $scope.$apply();
     //LOGOUT FUNCTOIN:
    $scope.logOut = function (){
-     if ($scope.loginData.password == undefined){$scope.matn="رمزتو بده اول خو:|";return;}
+     if (storage.getItem("savedpass") == undefined){$scope.matn="رمزتو بده اول خو:|";return;}
      $scope.matn="در حال باربری.-.-\.-/.-\.-/.-\.-/";if (!$scope.$$phase) $scope.$apply();
      console.log($scope.loginData.username,$scope.loginData.password);
+     // console.log(storage.getItem("savedusername"),storage.getItem("savedpass");
      window.cordovaHTTP.setHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.3; C5303 Build/12.1.A.1.205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
    //sakhte ibs_sesssid:
     window.cordovaHTTP.get("http://vpn-report.um.ac.ir/IBSng/user/",{},{},function(response){
     window.cordovaHTTP.setHeader("Cookie",response.headers["Set-Cookie"]);
     window.cordovaHTTP.post("http://vpn-report.um.ac.ir/IBSng/user/",{
-    normal_username:$scope.loginData.username,
-    normal_password:$scope.loginData.password,
+    normal_username:storage.getItem("savedusername"),
+    normal_password:storage.getItem("savedpass"),
     lang:"en",
     x:"12",
     y:"12"
@@ -93,32 +96,34 @@ function parseHTML(html) {
          unique_id_val:negUniqID
        },
        {},
-       function(response){console.log(response.status);$scope.matn="3 "+response.status;if (!$scope.$$phase) $scope.$apply();
+       function(response){console.log(response.status);$scope.matn="کد خطا: 3 "+response.status;if (!$scope.$$phase) $scope.$apply();
          $scope.matn="حله انداختیمش بیرون";if (!$scope.$$phase) $scope.$apply();
        },
-       function(response){console.log(response.status);$scope.matn="2 "+response.status;if (!$scope.$$phase) $scope.$apply();})   
+       function(response){console.log(response.status);$scope.matn="کد خطا: 2 "+response.status;if (!$scope.$$phase) $scope.$apply();})   
      },
-     function(response) {console.log(response.status);$scope.matn="1  "+response.status;if (!$scope.$$phase) $scope.$apply();}
+     function(response) {console.log(response.status);$scope.matn="نتونستم به سرور متصل شم.کد خطا:  "+response.status;if (!$scope.$$phase) $scope.$apply();}
      );
-  },function(response){})
+  },function(response){
+    $scope.matn="نتونستم به سرور متصل شم.کد خطا:"+response.status;if (!$scope.$$phase) $scope.$apply();
+  })
   //end of sakhte ibs_sesssid 
  };
  //END OF LOG OUT FUNCTION
 
   //LOGIN FUNCTION
     $scope.logInNet = function (){
-     if ($scope.loginData.password == undefined){$scope.matn="رمزتو بده اول خو:|";if (!$scope.$$phase) $scope.$apply();return;}
+     if (storage.getItem("savedpass") == undefined){$scope.matn="رمزتو بده اول خو:|";if (!$scope.$$phase) $scope.$apply();return;}
      window.cordovaHTTP.setHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.3; C5303 Build/12.1.A.1.205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
      window.cordovaHTTP.setHeader("Cookie","etag=undefined; cache=undefined");
      window.cordovaHTTP.post("https://hotspot.um.ac.ir/login",{
       dst :" ",
       popup:"true",
-      username:$scope.loginData.username,
-      password:$scope.loginData.password
+      username:storage.getItem("savedusername"),
+      password:storage.getItem("savedpass")
      },{},function(response){
       $scope.matn="OK!logged in:)";if (!$scope.$$phase) $scope.$apply();
      },function(response){
-      $scope.matn="نشد دوباره امتحان کن چون:";if (!$scope.$$phase) $scope.$apply();//add appropriate (:/) errors
+      $scope.matn="نشد دوباره امتحان کن چون:"+response.status;if (!$scope.$$phase) $scope.$apply();//add appropriate (:/) errors
      });
     };
   //END OF LOGIN 
