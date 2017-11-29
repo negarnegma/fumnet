736b2ab8c80037c47d579680f0ae1de787864594
negarnegma <negarzist@gmail.com>
2017-11-30 01:13:43 +0330

    simple errs showing (from response)

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,6 +16,8 @@ angular.module('starter.controllers', [])
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope
   }).then(function(modal) {
+    $scope.loginData.username = window.localStorage.getItem("savedusername");
+    $scope.loginData.password = window.localStorage.getItem("savedpass");
     $scope.modal = modal;
     $scope.closeLogin = function(){$scope.modal.hide();};
   });
@@ -62,51 +64,62 @@ function parseHTML(html) {
     $scope.currentStatus = "کللیک کن";if (!$scope.$$phase) $scope.$apply();
     //LOGOUT FUNCTOIN:
    $scope.logOut = function (){
-     if (storage.getItem("savedpass") == undefined){$scope.matn="رمزتو بده اول خو:|";return;}
-     $scope.matn="در حال باربری.-.-\.-/.-\.-/.-\.-/";if (!$scope.$$phase) $scope.$apply();
-     console.log($scope.loginData.username,$scope.loginData.password);
-     // console.log(storage.getItem("savedusername"),storage.getItem("savedpass");
-     window.cordovaHTTP.setHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.3; C5303 Build/12.1.A.1.205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
-   //sakhte ibs_sesssid:
-    window.cordovaHTTP.get("http://vpn-report.um.ac.ir/IBSng/user/",{},{},function(response){
-    window.cordovaHTTP.setHeader("Cookie",response.headers["Set-Cookie"]);
-    window.cordovaHTTP.post("http://vpn-report.um.ac.ir/IBSng/user/",{
-    normal_username:storage.getItem("savedusername"),
-    normal_password:storage.getItem("savedpass"),
-    lang:"en",
-    x:"12",
-    y:"12"
-    },
-  {},
-  function(response) {
-      console.log("resheaders:"+JSON.stringify(response.headers));
-      var kol = parseHTML (response.data);
-      var joz = kol.querySelector(".Main_Page .List_Main  form");$scope.matn="1"+response.status;if (!$scope.$$phase) $scope.$apply();
-      if (joz == undefined) {
-         $scope.matn="امانت شما آزاد است.";if (!$scope.$$phase) $scope.$apply();//حالت های مختلف رو اضافه کن..وارد نشدن و...
-         return;
-       }$scope.matn="2"+response.status;if (!$scope.$$phase) $scope.$apply();
-       console.log(joz.unique_id_val.value);
-       var negUniqID = parseInt(joz.unique_id_val.value);
-       console.log(joz.ras_ip.value);
-       var negRasIp = joz.ras_ip.value;
-       window.cordovaHTTP.post("http://vpn-report.um.ac.ir/IBSng/user/home.php",
-        {kill_me:1,
-         ras_ip:negRasIp,
-         unique_id_val:negUniqID
+       if (storage.getItem("savedpass") == undefined){$scope.matn="رمزتو بده اول خو:|";return;}
+       $scope.matn="در حال باربری.-.-\.-/.-\.-/.-\.-/";if (!$scope.$$phase) $scope.$apply();
+       console.log($scope.loginData.username,$scope.loginData.password);
+       // console.log(storage.getItem("savedusername"),storage.getItem("savedpass");
+       window.cordovaHTTP.setHeader("User-Agent", "Mozilla/5.0 (Linux; Android 4.3; C5303 Build/12.1.A.1.205) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.124 Mobile Safari/537.36");
+     //sakhte ibs_sesssid:
+      window.cordovaHTTP.get("http://vpn-report.um.ac.ir/IBSng/user/",{},{},function(response){
+      window.cordovaHTTP.setHeader("Cookie",response.headers["Set-Cookie"]);
+      window.cordovaHTTP.post("http://vpn-report.um.ac.ir/IBSng/user/",{
+      normal_username:storage.getItem("savedusername"),
+      normal_password:storage.getItem("savedpass"),
+      lang:"en",
+      x:"12",
+      y:"12"
+      },
+    {},
+    function(response) {
+        console.log("resheaders:"+JSON.stringify(response.headers));
+        var kol = parseHTML (response.data);
+        var joz = kol.querySelector(".Main_Page .List_Main  form");
+        if (joz == undefined) {
+          console.log(response.headers["Location"]);
+          $scope.matn="ERRor signing in IBS..";if (!$scope.$$phase) $scope.$apply();
+          joz = kol.querySelector(".error_messages");
+          console.log(joz.innerText);
+          if (joz!= undefined) $scope.matn=joz.innerText;
+          if (!$scope.$$phase) $scope.$apply();
+          return;
+         }
+         console.log(joz.unique_id_val.value);
+         var negUniqID = parseInt(joz.unique_id_val.value);
+         console.log(joz.ras_ip.value);
+         var negRasIp = joz.ras_ip.value;
+         $scope.matn="در حال بیرون انداختن..☼☺●◌ᴥ";if (!$scope.$$phase) $scope.$apply();
+         window.cordovaHTTP.post("http://vpn-report.um.ac.ir/IBSng/user/home.php",
+          {kill_me:1,
+           ras_ip:negRasIp,
+           unique_id_val:negUniqID
+         },
+         {},
+         function(response){
+           $scope.matn="حله انداختیمش بیرون";if (!$scope.$$phase) $scope.$apply();
+         },
+         function(response){
+          console.log(response.status);
+          $scope.matn="کد خطا: 2i"+response.status;if (!$scope.$$phase) $scope.$apply();//codeGozarie khataya:|2i404 :|
+        });   
        },
-       {},
-       function(response){console.log(response.status);$scope.matn="کد خطا: 3 "+response.status;if (!$scope.$$phase) $scope.$apply();
-         $scope.matn="حله انداختیمش بیرون";if (!$scope.$$phase) $scope.$apply();
-       },
-       function(response){console.log(response.status);$scope.matn="کد خطا: 2 "+response.status;if (!$scope.$$phase) $scope.$apply();})   
-     },
-     function(response) {console.log(response.status);$scope.matn="نتونستم به سرور متصل شم.کد خطا:  "+response.status;if (!$scope.$$phase) $scope.$apply();}
-     );
-  },function(response){
-    $scope.matn="نتونستم به سرور متصل شم.کد خطا:"+response.status;if (!$scope.$$phase) $scope.$apply();
-  })
-  //end of sakhte ibs_sesssid 
+       function(response) {
+        console.log(response.status);
+        $scope.matn="نتونستم به سرور متصل شم.کد خطا: 3i"+response.status;if (!$scope.$$phase) $scope.$apply();}
+       );
+    },function(response){
+      $scope.matn="نتونستم به سرور متصل شم.کد خطا: 4i"+response.status;if (!$scope.$$phase) $scope.$apply();
+    })
+    //end of sakhte ibs_sesssid 
  };
  //END OF LOG OUT FUNCTION
 
@@ -122,6 +135,12 @@ function parseHTML(html) {
       password:storage.getItem("savedpass")
      },{},function(response){
       $scope.matn="OK!logged in:)";if (!$scope.$$phase) $scope.$apply();
+       var kol = parseHTML (response.data);
+       var joz = kol.querySelectorAll("table div");//or selet : color = #FF8080 ...
+       var theErr = joz[1];
+       console.log(theErr);
+       if (theErr != undefined){$scope.matn=theErr.innerText;if (!$scope.$$phase) $scope.$apply();}
+
      },function(response){
       $scope.matn="نشد دوباره امتحان کن چون:"+response.status;if (!$scope.$$phase) $scope.$apply();//add appropriate (:/) errors
      });
